1b10211dfeee7d2db030b6a80b8c2f26494e39b3
Turbotor <63958625+Turbotor@users.noreply.github.com>
2020-05-24 21:17:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+// Q1
+// var firstVar;
+
+//Q2
+//var name = "Tor"; 
+
+//Q3
+//var int = 10;
+
+//Q4
+// var division = 20/5;
+
+//Q5
+//
+
+//Q6
+//var orderHasShipped = false;
+
+//Q7
+//var oderHasShipped = true;
+if(orderHasShipped === true) {
+    console.log("true"); 
+    }
+    else { 
+      console.log("false"); 
+      }
+      
+//Q8
+//for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
